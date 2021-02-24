50bb8a77403622b6f86be33950ce1666839f4b88
zerotower69 <zero@zerotower.xyz>
2021-02-25 02:24:50 +0800

    aa

---
+++ src/options/ClientOptions.ts
@@ -0,0 +1,36 @@
+import * as Type from '../types'
+// import * as Mysql from '@types/mysql'
+
+class ClientOptions implements Type.ClientOptions{
+  /**
+     * The MySQL user to authenticate as
+     */
+    user?: string;
+
+    /**
+     * The password of that MySQL user
+     */
+    password?: string;
+
+    /**
+     * Name of the database to use for this connection
+     */
+    database?: string;
+
+    /**
+     * The charset for the connection. This is called "collation" in the SQL-level of MySQL (like utf8_general_ci).
+     * If a SQL-level charset is specified (like utf8mb4) then the default collation for that charset is used.
+     * (Default: 'UTF8_GENERAL_CI')
+     */
+    charset?: string;
+
+    /**
+     * Number of milliseconds
+     */
+    timeout?: number;
+  construtor() {
+    
+  }
+}
+
+export =ClientOptions
